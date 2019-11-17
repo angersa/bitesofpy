86f0c6fb385b35f3de340c3a356c0f2747eaf36c
Annie Angers <annie.angers@umontreal.ca>
2019-11-17 12:44:14 -0500

    Bite138 solution

--- 154/dc.py
+++ 154/dc.py
@@ -1,17 +1,30 @@
+# from dataclasses import dataclass
+
+# @dataclass
+# class Bite:
+#     number: int
+#     title: str
+#     level: str
+
+#     def __init__(self, number, title, level='Beginner'):
+#         super().__init__()
+#         self.title = title.capitalize()
+#         self.number = number
+#         self.level = level
+
+#     def __lt__(self, other):
+#          return self.number < other.number
+
+# The solution is much simpler:
+
 from dataclasses import dataclass
 
-@dataclass
+
+@dataclass(order=True)
 class Bite:
     number: int
     title: str
-    level: str
+    level: str = 'Beginner'
 
-    def __init__(self, number, title, level='Beginner'):
-        super().__init__()
-        self.title = title.capitalize()
-        self.number = number
-        self.level = level
-
-    def __lt__(self, other):
-         return self.number < other.number
-
+    def __post_init__(self):
+        self.title = self.title.capitalize()
