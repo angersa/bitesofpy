049fa708017866f10c174296717c806036028f4f
Annie Angers <annie.angers@umontreal.ca>
2019-12-28 10:58:48 -0500

    Added the correct solution to bite 16

--- 16/gendates.py
+++ 16/gendates.py
@@ -15,3 +15,13 @@ def gen_special_pybites_dates():
         date = date + timedelta(days=365)
         special_dates.append(date)
     return sorted(special_dates)
+
+
+# Much better than mine... Did not follow the instructions well
+# (not a generator)
+def gen_special_pybites_dates_solution():
+    days = 0
+    while True:
+        days += 1
+        if days % 100 == 0 or days % 365 == 0:
+            yield PYBITES_BORN + timedelta(days=days)
