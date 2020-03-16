58bc82c9bbf8904053433c42f99a41ed5dff5146
Annie Angers <annie.angers@umontreal.ca>
2020-03-15 20:58:33 -0400

    corrected bite 43 solution

--- 43/kwargs.py
+++ 43/kwargs.py
@@ -1,2 +1,2 @@
-def get_profile(name:str = 'julian', profession:str='programmer') -> str:
+def get_profile(*, name: str = 'julian', profession: str= 'programmer') -> str:
     return f'{name} is a {profession}'
